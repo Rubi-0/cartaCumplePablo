7dd5d557500b59c96ebe78c7885132adac898a55
Rubi-0 <fatimarubitc04@gmail.com>
2026-09-10 01:38:15 -0600

    feat: crear pantalla inicial de carga

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("HTML, CSS y JavaScript conectados correctamente.");
+const loadingScreen = document.querySelector("#loading-screen");
+const saveScreen = document.querySelector("#save-screen");
+
+const progressTrack = document.querySelector(".progress-track");
+const progressBar = document.querySelector("#loading-progress");
+const progressPercentage = document.querySelector(
+    "#loading-percentage"
+);
+
+let progress = 0;
+
+const loadingInterval = setInterval(() => {
+    progress = Math.min(progress + 2, 100);
+
+    progressBar.style.width = `${progress}%`;
+    progressPercentage.textContent = `${progress}%`;
+
+    progressTrack.setAttribute("aria-valuenow", progress);
+
+    if (progress === 100) {
+        clearInterval(loadingInterval);
+
+        setTimeout(() => {
+            loadingScreen.hidden = true;
+            saveScreen.hidden = false;
+        }, 500);
+    }
+}, 100);
